fb9b5e8779ac81ddc2fd093c7245dcbddc1cd75a
DarkaivxInTheHouse <lil.kluy20@gmail.com>
2021-03-15 19:37:00 +0700

    Update 1.0.1

--- README.md
+++ README.md
@@ -1,23 +1,39 @@
-# System
+## Preview
+
+![Preview](https://cdn.discordapp.com/attachments/680320002666397705/820989705754902528/Screenshot_2021-03-15_Club3.png?width=1023&height=558)
+![Preview](https://cdn.discordapp.com/attachments/680320002666397705/820996183819288606/Screenshot_2021-03-15_Club7.png?width=1023&height=558)
+![Preview](https://cdn.discordapp.com/attachments/680320002666397705/820991530893705219/Screenshot_2021-03-15_Club6.png?width=1023&height=558)
+![Preview](https://cdn.discordapp.com/attachments/680320002666397705/820991422831394837/Screenshot_2021-03-15_Club5.png?width=1023&height=558)
+![Preview](https://cdn.discordapp.com/attachments/680320002666397705/820971616078921768/Screenshot_2021-03-15_Club.png?width=1023&height=558)
+![Preview](https://cdn.discordapp.com/attachments/680320002666397705/820971806575558666/Screenshot_2021-03-15_Club1.png?width=1023&height=558)
+
+## System ( Update 1.0.1 )
+#### หน้าบ้าน
  - ดูข้อมูลชุมนุม (เสร็จ)
  - ดูข้อมูลนักเรียน (เสร็จ)
  - ระบบสมัครชุมนุม (เสร็จ)
- - ระบบค้นหาชุมนุม (ยังไม่เสร็จ)
+ - โชว์นักเรียนในชุมนุม (เสร็จ)
+ - ระบบค้นหาชุมนุม (เสร็จ)
+
+#### หลังบ้าน
+
+ - เพิ่ม/ลบ นักเรียน/ชุมนุม (ยังไม่เสร็จ)
+ - จัดการ นักเรียน/ชุมนุม (ยังไม่เสร็จ)
 
 ทุกอย่างที่กล่าวมาข้างต้นจัดการด้วย API ทั้งหมด!
 
-# Install
+## Install
 
 ```cmd
 npm i
 node .
 ```
 
-# Credit
+## Credit
 
 Made with 💖 by [Da_aK](https://github.com/DarkavixInTheHouse)
 
-# Donate
+## Donate
 [on Truemoney Wallet](https://tmn.app.link/OlFK3AByCeb)
 or
 ![Donate](https://media.discordapp.net/attachments/680320002666397705/820660121163006002/USER_SCOPED_TEMP_DATA_orca-image--1126638565.jpeg)
--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const path = require("path")
+const { stringify } = require("querystring")
 const db = require("better-sqlite3")("main.db")
 const app = express()
-const session = require('express-session')
 
 app.set("views", __dirname + "/views")
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, "public")))
-
-app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-}))
 
 app.get("/", (req, res) => {
-    res.render("main")
+    if (req.query.s) {
+        res.render("search", {
+            search: String(req.query.s)
+        })
+    } else {
+        res.render("main", {
+            search: ""
+        })
+    }
 })
 
 let baseApiUrl = "/api/v1/"
@@ -49,7 +50,7 @@ app.get(baseApiUrl + "clubs/all", async (req, res) => {
         let data = await db.prepare("SELECT id, name, firstname_teacher, lastname_teacher, max FROM clubs ORDER BY id").all()
         for (let obj of data) {
             let users = await db.prepare("SELECT * FROM users WHERE club = " + obj.id).all()
-            obj["min"] = users.length
+            obj["members"] = users
         }
         res.send({
             error: false,
@@ -60,7 +61,7 @@ app.get(baseApiUrl + "clubs/all", async (req, res) => {
         let data = await db.prepare("SELECT id, name, firstname_teacher, lastname_teacher, max FROM clubs ORDER BY id LIMIT " + limit).all()
         for (let obj of data) {
             let users = await db.prepare("SELECT * FROM users WHERE club = " + obj.id).all()
-            obj["min"] = users.length
+            obj["members"] = users
         }
         res.send({
             error: false,
@@ -83,10 +84,62 @@ app.get(baseApiUrl + "clubs/:id", async (req, res) => {
         msg: "ไม่พบชุมนุมนี้!",
         data: [],
     })
+    let users = await db.prepare("SELECT * FROM users WHERE club = " + data.id).all()
+    data["members"] = users
     res.send({
         error: false,
         msg: "Good Job!",
         data,
+    })
+})
+
+app.get(baseApiUrl + "clubs/search/:val", async (req, res) => {
+    let val = req.params.val
+    if (!val) return res.send({
+        error: true,
+        msg: "Bad Request!",
+        data: [],
+    })
+    let data = await db.prepare("SELECT id, name, firstname_teacher, lastname_teacher, max FROM clubs WHERE id LIKE ? OR name LIKE ?").all(Number(val), `%${String(val)}%`)
+    if (!data || data.length <= 0) return res.send({
+        error: true,
+        msg: "ไม่พบชุมนุม!",
+        data: [],
+    })
+    for (let obj of data) {
+        let users = await db.prepare("SELECT * FROM users WHERE club = " + obj.id).all()
+        obj["members"] = users
+    }
+    res.send({
+        error: false,
+        msg: "Good Job!",
+        data,
+    })
+})
+
+app.get(baseApiUrl + "clubs/:id/members", async (req, res) => {
+    let id = Number(req.params.id)
+    if (!id) return res.send({
+        error: true,
+        msg: "Bad Request!",
+        data: [],
+    })
+    let club = await db.prepare("SELECT id, name, firstname_teacher, lastname_teacher, max FROM clubs WHERE id = ?").get(id)
+    if (!club) return res.send({
+        error: true,
+        msg: "ไม่พบชุมนุมนี้!",
+        data: [],
+    })
+    let users = await db.prepare(`SELECT id, firstname, lastname, studentnumber, club, class FROM users WHERE club = ${club.id} ORDER BY id`).all()
+    if (!users || users.length <= 0) return res.send({
+        error: true,
+        msg: "ไม่พบนักเรียนในชุมนุมนี้!",
+        data: [],
+    })
+    res.send({
+        error: false,
+        msg: "Good Job!",
+        data: users,
     })
 })
 
@@ -99,24 +152,24 @@ app.post(baseApiUrl + "clubs/joinclub", async (req, res) => {
         data: [],
     })
     let user = await db.prepare("SELECT * FROM users WHERE id = ?").get(id)
-    if(!user) return res.send({
+    if (!user) return res.send({
         error: true,
         msg: "ไม่พบนักเรียนนี้!",
         data: [],
     })
-    if(!user.club == null || !user.club == 0) return res.send({
+    if (!user.club == null || !user.club == 0) return res.send({
         error: true,
         msg: "คุณมีชุมนุมอยู่แล้ว!",
         data: [],
     })
     let club = await db.prepare("SELECT * FROM clubs WHERE id = ?").get(cid)
-    if(!club) return res.send({
+    if (!club) return res.send({
         error: true,
         msg: "ไม่พบชุมนุมนี้!",
         data: [],
     })
-    let users = await db.prepare("SELECT * FROM users WHERE club = " + club.id).all()
-    if(users.length >= club.max) return res.send({
+    let users = await db.prepare(`SELECT * FROM users WHERE club = ${club.id}`).all()
+    if (users.length >= club.max) return res.send({
         error: false,
         msg: "ชุมนุมนี้เต็มแล้ว!",
         data: [],
